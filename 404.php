ab0efdfbe0f2b807997b1e35917da689baad0026
wkey1980 <28814921+wkey1980@users.noreply.github.com>
2021-03-03 14:51:25 +0000

    created 404.php

--- 404.php
+++ 404.php
@@ -9,6 +9,11 @@
 
 <?php get_header(); ?>
 
+<section class="error-404 not-found">
+    <h1><?php esc_attr_e( '4', 'iggy' ); ?><?php esc_html_e( '4', 'iggy' ); ?></h1>
+    <h1><?php esc_html_e( 'No Content Found', 'iggy' ); ?></h1>
 
+    <a href="<?php echo esc_url( home_url() ); ?>">Go Home!</a>
+</section>
 
 <?php get_footer(); ?>
