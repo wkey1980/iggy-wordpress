708dd3f0768078b5c9af0c26d59c1876e995b704
wkey1980 <28814921+wkey1980@users.noreply.github.com>
2021-03-03 15:13:27 +0000

    Added bootstrap source files & enqueue styles/scripts

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -6,3 +6,23 @@
  * @package iggy
  */
 ?>
+<?php
+
+function iggy_enqueue_scripts() {
+
+    // Register Bootstrap Scripts
+    wp_register_script( 'bootstrap-js', IGGY_THEME_URI . '/assets/library/src/bootstrap/js/bootstrap.bundle.js', array( 'jquery' ), false, true );
+
+
+    // Register Theme Scripts
+    wp_register_script( 'theme-js', IGGY_THEME_URI . '/assets/js/theme.js', array(), false, 'all' );  // * NEED TO ADD TIME STAMP
+
+
+    // Enqueue Bootstrap Scripts
+    wp_enqueue_script( 'bootstrap-js' );
+
+
+    // Enqueue Theme Scripts
+    wp_enqueue_script( 'theme-js' );
+}
+add_action( 'wp_enqueue_scripts', 'iggy_enqueue_scripts' );
--- inc/enqueue-styles.php
+++ inc/enqueue-styles.php
@@ -6,3 +6,25 @@
  * @package iggy
  */
 ?>
+
+<?php
+
+function iggy_enqueue_styles() {
+
+    // Register Bootstrap Styles
+    wp_register_style( 'bootstrap-style', IGGY_THEME_URI . '/assets/library/src/bootstrap/css/bootstrap.css', array(), false, 'all' );
+
+
+    // Register Theme Styles
+    wp_register_style( 'theme-style', IGGY_THEME_URI . '/assets/css/theme.css', array(), false, 'all' ); // * NEED TO ADD TIME STAMP
+
+
+    // Enqueue Bootstrap Styles
+    wp_enqueue_style( 'bootstrap-style' );
+
+
+    // Enqueue Theme Styles
+    wp_enqueue_style( 'theme-style' );
+
+}
+add_action( 'wp_enqueue_scripts', 'iggy_enqueue_styles' );
